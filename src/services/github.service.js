b30e88c63047465f6d43c8771e56e131601425c2
snstrauss <zahavi.guy.85@gmail.com>
2020-06-09 15:19:56 +0300

    moved custom git hooks logic to github.service

--- src/services/github.service.js
+++ src/services/github.service.js
@@ -1,16 +1,40 @@
 "https://api.github.com/repos/snstrauss/git-cookin-db/contents/recipes"
 
 const BASE_URL = "https://api.github.com";
-const AUTH_TOKEN = "b9ef1a4569faf4b62a685f2020a83cb4fcb59b22";
 
 const headers = new Headers();
 headers.append("Accept", "application/vnd.github.v3+json");
-headers.append("Authorization", `token ${AUTH_TOKEN}`);
+headers.append("Authorization", `token ${process.env.REACT_APP_GITHUB_TOKEN}`);
 
 export function getFilesContent({ owner = 'snstrauss', repository = 'git-cookin-db', path = '' }){
-    return fetch(`${BASE_URL}/repos/${owner}/${repository}/contents/${path}`, {
+    return fetch(`${BASE_URL}/repos/${owner}/${repository}/contents/recipes/${path}`, {
         headers
     }).then((response) => response.json());
+}
+
+
+export function getRecipesList(){
+
+    return getFilesContent({
+        path: '',
+    }).then(mapRecipeList);
+}
+
+export function getRecipeData(path){
+    return getFilesContent({
+        path
+    }).then(parseFileContent);
+}
+
+function mapRecipeList(recipes){
+    return recipes.map(({ name, download_url }) => ({
+        url: download_url,
+        name
+    }));
+}
+
+function parseFileContent(fileData){
+    return JSON.parse(atob(fileData.content));
 }
 
 export default {
