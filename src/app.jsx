5c3dbc735769e9034585d5379b043615203d7a23
snstrauss <zahavi.guy.85@gmail.com>
2020-06-09 16:49:08 +0300

    created UserContext that keeps the name of the current user

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,24 @@
-import React, { useState } from "react"
+import React, { useState, createContext } from "react"
 import RecipeList from "./components/recipe-list/recipe-list";
+import Login from "./components/login/login";
 
-
+export const UserContext = createContext();
 
 export default function App() {
 
   const [currentUser, setCurrentUser] = useState();
 
   return (
-    <RecipeList currentUser={currentUser}  />
+    <UserContext.Provider value={{
+      currentUser
+    }}>
+      {
+        currentUser
+        ?
+        <RecipeList/>
+        :
+        <Login setUser={setCurrentUser}/>
+      }
+    </UserContext.Provider>
   )
 }
