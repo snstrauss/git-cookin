8eb3117533a037783088b450d0054aca2acf3455
snstrauss <zahavi.guy.85@gmail.com>
2020-06-11 11:57:42 +0300

    app now wraps the main content (<RecipeList>) with <UserContextGiver>, to provide the entire app with data of user, and show login page if needed.

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,12 @@
-import React, { useState, createContext } from "react"
-import RecipeList from "./components/recipe-list/recipe-list";
-import Login from "./components/login/login";
-
-export const UserContext = createContext();
+import React from "react"
+import UserContextGiver from "./contexts/userContext/userContext";
+import RecipeList from "./components/main/recipe-list/recipe-list";
+import './global.scss';
 
 export default function App() {
-
-  const [currentUser, setCurrentUser] = useState();
-
   return (
-    <UserContext.Provider value={{
-      currentUser
-    }}>
-      {
-        currentUser
-        ?
-        <RecipeList/>
-        :
-        <Login setUser={setCurrentUser}/>
-      }
-    </UserContext.Provider>
-  )
+    <UserContextGiver>
+      <RecipeList/>
+    </UserContextGiver>
+  );
 }
