96b758730e897c199b0760b4da8c2df317510a3a
snstrauss <zahavi.guy.85@gmail.com>
2020-06-11 11:53:24 +0300

    created a <UserContextGiver> component, to manage to login process, and provide all of it's children with methods to logout and get current user name.

---
+++ src/contexts/userContext/userContext.jsx
@@ -0,0 +1,51 @@
+import React, { useState, createContext, useEffect } from 'react';
+import Logout from '../../components/utils/login/logout';
+import Login from '../../components/utils/login/login';
+
+export const UserContext = createContext();
+
+export default function UserContextGiver({ children }){
+
+    const [currentUser, setCurrentUser] = useState();
+
+    const context = {
+        logout(){
+            keepAndSetUsername();
+        },
+        getCurrentUser(){
+            return currentUser
+        }
+    };
+
+    useEffect(() => {
+        const currentUsername = localStorage.currentUser;
+
+        if(currentUsername){
+            keepAndSetUsername(currentUsername);
+        }
+    }, []);
+
+    function keepAndSetUsername(username){
+        if(username){
+            localStorage.currentUser = username;
+        } else {
+            localStorage.removeItem('currentUser');
+        }
+        setCurrentUser(username);
+    }
+
+    return (
+        <UserContext.Provider value={context}>
+            {
+                currentUser
+                ?
+                <>
+                    {children}
+                    <Logout/>
+                </>
+                :
+                <Login setUser={keepAndSetUsername}/>
+            }
+        </UserContext.Provider>
+    );
+}
