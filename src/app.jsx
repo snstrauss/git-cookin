f05edc67dd83ad7df123e1000c3b81c936a73667
snstrauss <zahavi.guy.85@gmail.com>
2020-06-15 15:23:05 +0300

    add <Layout> component to app

--- src/app.jsx
+++ src/app.jsx
@@ -2,11 +2,14 @@ import React from "react"
 import UserContextGiver from "./contexts/userContext/userContext";
 import RecipeList from "./components/main/recipe-list/recipe-list";
 import './global.scss';
+import Layout from "./components/main/layout/layout";
 
 export default function App() {
   return (
     <UserContextGiver>
-      <RecipeList/>
+      <Layout>
+        <RecipeList/>
+      </Layout>
     </UserContextGiver>
   );
 }
