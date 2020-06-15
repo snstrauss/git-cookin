e015632dffc04840233a5043343219087154a738
snstrauss <zahavi.guy.85@gmail.com>
2020-06-15 15:20:50 +0300

    <Layout> component is now in charge of showing the <Login> component, instead of User.context

--- src/components/main/layout/layout.jsx
+++ src/components/main/layout/layout.jsx
@@ -1,27 +1,33 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useContext } from 'react';
 import S from './layout.module.scss';
 import Header from '../../partials/header/header';
+import { UserContext } from '../../../contexts/userContext/userContext';
+import Login from '../../partials/login/login';
 
 export const LayoutContext = createContext();
 
 export default function Layout({ children }){
 
-    const [headerTitle, setHeaderTitle] = useState("git cookin'!");
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [headerTitle, setHeaderTitle] = useState();
 
     const context = {
-        setHeaderTitle,
-        setIsLoggedIn
-    }
+        setHeaderTitle
+    };
 
-    debugger;
+    const { currentUser } = useContext(UserContext);
 
     return (
         <div className={S.container}>
             <LayoutContext.Provider value={context}>
-                <Header title={headerTitle} isLoggedIn={isLoggedIn}/>
+                <Header title={headerTitle}/>
                 <main>
-                    {children}
+                    {
+                        currentUser
+                        ?
+                        children
+                        :
+                        <Login/>
+                    }
                 </main>
                 <footer></footer>
             </LayoutContext.Provider>
--- src/components/partials/login/login.jsx
+++ src/components/partials/login/login.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import S from './login.module.scss';
+import { UserContext } from '../../../contexts/userContext/userContext';
 
-export default function Login({ setUser }){
+export default function Login(){
+
+    const { login } = useContext(UserContext);
+
     return (
         <div className={S.container}>
-            <form onSubmit={((ev) => setUser(ev.target.elements.username.value))}>
+            <form onSubmit={((ev) => login(ev.target.elements.username.value))}>
                 <input name="username" type="text" placeholder="username..?" />
                 <input type="submit" value="Go"/>
             </form>
